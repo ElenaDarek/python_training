1a97174a49c35ba2fbcc57b3978e95888383037a
ElenaDarek <elenadarek@gmail.com>
2018-06-13 22:02:52 +0300

    Настройка кеширования и хеширования в тестах

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,22 +8,22 @@ def test_add_group(app):
     app.group.open_groups_page()
     app.group.create(group)
     app.group.return_to_group_page()
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.open_groups_page()
-    app.group.create(group)
-    app.group.return_to_group_page()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#   old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#   app.group.open_groups_page()
+#    app.group.create(group)
+#    app.group.return_to_group_page()
+#    new_groups = app.group.get_group_list()
+#   assert len(old_groups) + 1 == len(new_groups)
+#   old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 

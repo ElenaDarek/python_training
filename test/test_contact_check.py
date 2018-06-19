bee069d611903e4fe2ea0b02e88eeec89f3cfabd
ElenaDarek <elenadarek@gmail.com>
2018-06-20 00:51:23 +0300

    Добавлены проверки для всех полей контакта на главной странице

--- test/test_contact_check.py
+++ test/test_contact_check.py
@@ -6,21 +6,16 @@ from model.contact import Contact
 def test_contact_check(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Elena", lastname="Darek", id="1", address="Test", homephone="89210000000",
-                      mobilephone="89210000000", workphone="89210000000",secondaryphone="89210000000",
-                                   email="elenadarek@gmail.com", email2="test1@mail.com",
-                      email3="test2@gmail.com"))
-
+                                   mobilephone="89210000000", workphone="89210000000",secondaryphone="89210000000",
+                                   email="elenadarek@gmail.com", email2="test1@mail.com", email3="test2@gmail.com"))
     all_contacts = app.contact.get_contact_list()
     index = randrange(len(all_contacts))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def clear(s):

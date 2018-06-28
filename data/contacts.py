02a60989b54ae38575d8324aa6b330f3825698e8
ElenaDarek <elenadarek@gmail.com>
2018-06-28 23:15:11 +0300

    Реализована динамическая генерация тестов

---
+++ data/contacts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8-*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname1", lastname="lastname1", address="address1", homephone="homephone1",
+                    mobilephone="mobilephone1", workphone="workphone1", secondaryphone="secondaryphone1",
+                    email="email1"),
+    Contact(firstname="firstname2", lastname="lastname2", address="address2", homephone="homephone2",
+                    mobilephone="mobilephone2", workphone="workphone2", secondaryphone="secondaryphone2",
+                    email="email2"),
+]
+
